d13c644fed949d72f7d92ed9a949111f7c642eb7
Anaë Yuurei <eaudrey96@gmail.com>
2019-12-05 23:57:41 +0100

    Added ParameterLister, this will allow me, in a near future, to make list of regular parameter used by tiktok.

---
+++ ParameterLister/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ParameterLister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"((?:[a-z]+|_|-)*)=((?:[a-zA-Z]+|[0-9]+|_|-|\.|\%|\*)*)";
+            Console.WriteLine("Print argument list here:");
+            var lolipop = Console.ReadLine();
+            RegexOptions options = RegexOptions.Multiline;
+
+            var var1 = Regex.Matches(lolipop, pattern, options);
+            var argList = new List<string>();
+            for (int i = 0; i < var1.Count; i++)
+            {
+                argList.Add(var1[i].Groups[1].Value);
+            }
+            File.AppendAllLines("arg", argList);
+        }
+    }
+}
