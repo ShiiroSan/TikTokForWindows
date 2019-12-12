e08ce3e785de187318639bfcc778c2fd40221a7d
Anaë Yuurei <eaudrey96@gmail.com>
2019-12-12 14:04:08 +0100

    Added sweetness.

--- ParameterLister/Program.cs
+++ ParameterLister/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -10,18 +9,93 @@ namespace ParameterLister
     {
         static void Main(string[] args)
         {
+            List<string> BagOfCandies = new List<string>();
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hoy Captain! We have some parameter there!");
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("Urm Captain, only one in fact!");
+                    string sadNSoloParameter = args[0];
+                    if (IsValidPath(sadNSoloParameter).IsFile)
+                    {
+                        Console.WriteLine("That was a file in fact!");
+                        string[] BagOfBiscuit = File.ReadAllLines(sadNSoloParameter);
+                        foreach (var Twix in BagOfBiscuit)
+                        {
+                            BagOfCandies.Add(Twix);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("... Only one link there!");
+                        BagOfCandies.Add(sadNSoloParameter);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Captain! Many parameter there!");
+                    var TheCrewOfParam = args;
+                    foreach(var soloParam in TheCrewOfParam)
+                    {
+                        Console.WriteLine("They even put file on it!");
+                        if (IsValidPath(soloParam).IsFile)
+                        {
+                            string[] BagOfBiscuit = File.ReadAllLines(soloParam);
+                            foreach (var Mars in BagOfBiscuit)
+                            {
+                                BagOfCandies.Add(Mars);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tiny link there!");
+                            BagOfCandies.Add(soloParam);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Print argument list here:");
+                BagOfCandies.Add(Console.ReadLine());
+            }
             string pattern = @"((?:[a-z]+|_|-)*)=((?:[a-zA-Z]+|[0-9]+|_|-|\.|\%|\*)*)";
-            Console.WriteLine("Print argument list here:");
-            var lolipop = Console.ReadLine();
             RegexOptions options = RegexOptions.Multiline;
 
-            var var1 = Regex.Matches(lolipop, pattern, options);
-            var argList = new List<string>();
-            for (int i = 0; i < var1.Count; i++)
+            foreach(string candie in BagOfCandies)
             {
-                argList.Add(var1[i].Groups[1].Value);
+                var Lollipops = Regex.Matches(candie, pattern, options);
+                var argList = new List<string>();
+                var valList = new List<string>();
+                foreach (Match lollipop in Lollipops)
+                {
+                    argList.Add(lollipop.Groups[1].Value);
+                }
             }
-            File.AppendAllLines("arg", argList);
+        }
+
+        static (string URI, bool IsAbsoluteUri, bool IsFile) IsValidPath(string path) //based on drzaus on stackoverflow
+        {
+            Uri u;
+            try
+            {
+                u = new Uri(path);
+            }
+            catch (Exception)
+            {
+                return (
+                    URI: path,
+                    IsAbsoluteUri: false,
+                    IsFile: true
+                );
+            }
+
+            return (
+                    URI: path,
+                    u.IsAbsoluteUri,
+                    u.IsFile
+                );
         }
     }
 }
